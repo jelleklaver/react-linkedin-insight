156fb570b15928fdbc97a27d5eff71d229bf99ba
Jelle Klaver <jelle.klaver@gmail.com>
2019-06-20 14:00:45 +0200

    Decrease npm package size

--- webpack.config.dist.js
+++ webpack.config.dist.js
@@ -2,7 +2,6 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-  devtool: "cheap-module-source-map",
   entry: ["./src/index.js"],
   output: {
     path: path.join(__dirname, "dist"),
