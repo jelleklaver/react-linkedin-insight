e11ee3f0cec20774c98922519e852c6fb71bcfa4
Jelle Klaver <jelle.klaver@gmail.com>
2020-11-23 11:46:40 +0100

    Upgrade deps and simplify babel code generator

--- webpack.config.dist.js
+++ webpack.config.dist.js
@@ -24,9 +24,7 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("production")
-      }
+      "process.env": { NODE_ENV: "production" }
     }),
     new webpack.LoaderOptionsPlugin({
       minimize: true,
