49cdb274d7450804f7d5e32671a679865bae11eb
Jelle Klaver <jelle.klaver@gmail.com>
2021-07-07 19:40:42 +0200

    Updated types to reflect usage (#20)

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,6 +1,16 @@
-function warn(...args: any): void;
-export function verifyInit(): boolean;
-export function init(partnerId: string, subDomain?: string, disabled?: boolean): void;
-export function track(conversionId?: string, partnerId?: string, subDomain?: string): void;
+export class LinkedInTag {
+    initialized: boolean;
+    disabled: boolean;
+    partnerId: string;
+    subDomain: string;
 
-export default LinkedInTag;
+    constructor();
+
+    warn(...args: any): void;
+    verifyInit(): boolean;
+    init(partnerId: string, subDomain?: string, disabled?: boolean): void;
+    track(conversionId?: string, partnerId?: string, subDomain?: string): void;
+  }
+
+  declare var instance: LinkedInTag;
+  export default instance;
